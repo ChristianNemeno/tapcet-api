c93eced89934784f5379791e7515fb6004df1cdb
christian <christiannemeno80@gmail.com>
2025-11-08 14:22:18 +0800

    in this checkpoint i added, I Auth service interface, I think this will serves as my contract or behaviour for Authentication

---
+++ tapcet-api/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using tapcet_api.DTO.Auth;
+
+namespace tapcet_api.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto?> RegisterAsync(RegisterDto registerDto);
+        Task<AuthResponseDto?> LoginAsync(LoginDto loginDto);
+        Task<bool> UserExistsAsync(string email);
+    }
+}
