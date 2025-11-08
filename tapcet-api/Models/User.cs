47d07ea1326642a2be419d3c4668d1b0516a2ef7
christian <christiannemeno80@gmail.com>
2025-11-08 12:34:52 +0800

    User model , using the identity user , some built in Net eco
    added dependencies,
    
    and data base connection

---
+++ tapcet-api/Models/User.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace tapcet_api.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public int TotalQuizAttempts { get; set; } = 0;
+        public double AverageScore { get; set; } = 0.0;
+
+    }
+}
