e44d68d0a2f0adf5ae9a8d0eecf556275c01f3ae
christian <christiannemeno80@gmail.com>
2025-11-08 13:47:30 +0800

    ApplicationDbContext, extends to IdentityDbContent , built in auth from , Entity framework i think?
    i commented some possible methods and notes

---
+++ tapcet-api/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using tapcet_api.Models;
+
+namespace tapcet_api.Data   
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets for quiz-related entities will be added later
+        // public DbSet<Quiz> Quizzes { get; set; }
+        // public DbSet<Question> Questions { get; set; }
+        // public DbSet<Choice> Choices { get; set; }
+        // public DbSet<QuizAttempt> QuizAttempts { get; set; }
+        // public DbSet<UserAnswer> UserAnswers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Additional configurations will be added here
+            // when we implement quiz models
+        }
+    }
+}
