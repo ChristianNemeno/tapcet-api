fb41e26969f4ed64217d379d0fa2bd769ffe2039
christian <christiannemeno80@gmail.com>
2026-01-14 22:36:34 +0800

    always 1 admin acc

---
+++ tapcet-api/Data/DbSeeder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using tapcet_api.Models;
+
+namespace tapcet_api.Data;
+
+public static class DbSeeder
+{
+    public static async Task SeedAsync(IServiceProvider services)
+    {
+        using var scope = services.CreateScope();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+
+        
+        string[] roleNames = { "Admin", "User" };
+        foreach (var roleName in roleNames)
+        {
+            if (!await roleManager.RoleExistsAsync(roleName))
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+
+        var adminEmail = "admin@tapcet.com";
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            adminUser = new User { UserName = "admin", Email = adminEmail, CreatedDate = DateTime.UtcNow };
+            await userManager.CreateAsync(adminUser, "Admin@123");
+            await userManager.AddToRoleAsync(adminUser, "Admin");
+        }
+    }
+}
